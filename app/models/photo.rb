52781502b4608ab671efc2f7b229097d3bfd941e
Martin Ubillus <martin.v.ubillus@gmail.com>
2022-04-21 21:52:32 -0500

    added counter for category

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,5 @@ class Photo < ApplicationRecord
   validates :subtitle, presence: true, length: { in: 20..500 }
   validates :comments_count, presence: true
 
-  belongs_to :category
+  belongs_to :category, counter_cache: true
 end
