966162f48b71e6d5ccf6dcbadb9cc407fa941c2b
fabio4520 <fabioleofc@gmail.com>
2022-04-21 19:59:03 -0500

    ADD: counter column to Photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_22_005726) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_22_005817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_22_005726) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_count"
     t.index ["category_id"], name: "index_photos_on_category_id"
   end
 
