76e9d17b533aa7c9590b0aca33ab0256a8318f07
fabio4520 <fabioleofc@gmail.com>
2022-04-21 21:28:03 -0500

    ADD: callbacks for comments_count in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
-  validates :body, presence:true, length: { in: 10..500 }
+  validates :body, presence: true, length: { in: 10..500 }
+  belongs_to :photo, counter_cache: nombre_random
 end
