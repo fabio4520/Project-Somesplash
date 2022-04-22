15f0aeef0c1920583742cd21e29640c877de46ab
fabio4520 <fabioleofc@gmail.com>
2022-04-22 17:39:27 -0500

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/category", to: "category#index"
-  get "/photo", to: "photo#index"
-  # creo que deberiamos tener un 
-  get "/category-edit", to: "category#edit_category"
-
+  get "/categories", to: "categories#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
