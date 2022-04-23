de294ce446883400e0d342feb7427355163a866b
yamathe <manuelsegmar@gmail.com>
2022-04-23 12:51:45 -0500

    FIX: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/categories", to: "categories#index"
+  resources :categories
+  resources :photos
   # Defines the root path route ("/")
   # root "articles#index"
 end
